e790ea284211129e8146b62d4d67df44a38d5b74
NewI3e <abdullahkhan145896@gmail.com>
2026-05-25 15:17:17 +0600

    error handeling

--- e-commerce-mern-project/server/src/server.js
+++ e-commerce-mern-project/server/src/server.js
@@ -3,15 +3,18 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const islogin = (req, res, next) => {
-  const login = false;
-  if (login == true) {
-    next();
-  } else {
-    return res.status(401).send("please log in");
-  }
-};
+// const islogin = (req, res, next) => {
+//   const login = true;
+//   if (login == true) {
+//     req.id = 101;
+//     next();
+//   } else {
+//     return res.status(401).send("please log in");
+//   }
+// };
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -19,10 +22,24 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/test", islogin, (req, res) => {
-  res.status(200).send("api /test is working");
-});
+app.post(
+  "/test",
+  /* islogin, */ (req, res) => {
+    console.log(req.id);
+    res.status(200).json({ massage: "api /test is working" });
+  },
+);
 
+//clint error handilg
+app.use((req, res, next) => {
+  res.status(404).json({ massage: "route not found" });
+  next();
+});
+//server error
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 app.listen(4000, () => {
   console.log("the server is running at http://localhost:4000");
 });
